e79adb64a667179f07f713b978e03617c750834d
Primary <yw3726@nyu.com>
2019-11-04 17:04:45 -0500

    Changed plane prefabs, tagged them players

--- Assets/Scripts/PlayerPlaneMovement.cs
+++ Assets/Scripts/PlayerPlaneMovement.cs
@@ -9,6 +9,7 @@ public class PlayerPlaneMovement : MonoBehaviour
     public float RetractSpeed;
     public float RotateSpeed;
     public float Rotation;
+    public float RotationGoal;
     public bool TiltingL;
     public bool TiltingR;
     public bool Boosting;
@@ -19,6 +20,10 @@ public class PlayerPlaneMovement : MonoBehaviour
     public bool Somersaulting;
     public bool Occupied;
 
+    public float InputBuffer;
+    public string LoadedInput;
+    public float RollTimer;
+
     void Awake()
     {
 
@@ -28,6 +33,8 @@ public class PlayerPlaneMovement : MonoBehaviour
     void Start()
     {
         FreeToMove();
+        InputBuffer = 0;
+        RollTimer = 0;
     }
 
     public void FreeToMove()
@@ -53,70 +60,192 @@ public class PlayerPlaneMovement : MonoBehaviour
         {
             return false;
         }
-    } 
+    }
 
     void Update()
     {
-        if (!UTurning && !Somersaulting)
+
+
+        if (InputBuffer > 0)
         {
+            InputBuffer -= Time.deltaTime;
+        }
+        else
+        {
+            InputBuffer = 0;
+            LoadedInput = null;
+        }
+
+        if (RollTimer > 0)
+        {
+            if (RollingL)
+            {
+                Rotation += RotateSpeed * 50 * Time.deltaTime;
+                RollTimer -= Time.deltaTime;
+                if (RollTimer <= 0)
+                {
+                    RollTimer = 0;
+                    RollingL = false;
+                    LoadedInput = "Null";
+                    Rotation = transform.localEulerAngles.z - 360;
+                }
+            }
+            else if (RollingR)
+            {
+                Rotation -= RotateSpeed * 50 * Time.deltaTime;
+                RollTimer -= Time.deltaTime;
+                if (RollTimer <= 0)
+                {
+                    RollTimer = 0;
+                    RollingR = false;
+                    LoadedInput = "Null";
+                    Rotation = transform.localEulerAngles.z;
+                }
+            }
+
+            transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, Rotation);
+        }
+
+
+        if (!RollingL && !RollingR && !UTurning && !Somersaulting)
+        {
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                if (LoadedInput != "TiltingL")
+                {
+                    InputBuffer = 0.5f;
+                    LoadedInput = "TiltingL";
+                }
+                else if (LoadedInput == "TiltingL" && InputBuffer > 0)
+                {
+                    RollingL = true;
+                    RollTimer = 0.5f;
+                }
+            }
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                if (LoadedInput != "TiltingR")
+                {
+                    InputBuffer = 0.5f;
+                    LoadedInput = "TiltingR";
+                }
+                else if (LoadedInput == "TiltingR" && InputBuffer > 0)
+                {
+                    
+                    RollingR = true;
+                    RollTimer = 0.5f;
+                }
+            }
+            
+            if (Input.GetKey(KeyCode.Q) && !TiltingR)
+            {
+                Occupied = true;
+                TiltingR = false;
+                TiltingL = true;
+                RotationGoal = 90;
+            }
+            else if (Input.GetKey(KeyCode.E) && !TiltingL)
+            {
+                Occupied = true;
+                TiltingL = false;
+                TiltingR = true;
+                RotationGoal = -90;
+            }
+            else
+            {
+                Occupied = false;
+                TiltingL = false;
+                TiltingR = false;
+            }
+
             if (Input.GetKey(KeyCode.RightArrow))
             {
                 if (FreeTilt())
                 {
-                    if (Rotation < -30)
-                    {
-                        Rotation = -30;
-                    }
+                    RotationGoal = -30;
                 }
-                Rotation -= RotateSpeed * Time.deltaTime;
             }
             else if (Input.GetKey(KeyCode.LeftArrow))
             {
                 if (FreeTilt())
                 {
-                    if (Rotation > 30)
-                    {
-                        Rotation = 30;
-                    }
+                    RotationGoal = 30;
                 }
-                Rotation += RotateSpeed * Time.deltaTime;
             }
-            else if (Rotation > 0 && Rotation <= 180)
+            else
             {
-                Rotation -= RetractSpeed * Time.deltaTime;
-                if (Rotation < 0)
+                if (FreeTilt())
                 {
-                    Rotation = 0;
+                    RotationGoal = 0;
                 }
             }
-            else if (Rotation < 0 && Rotation >= -180)
+
+            if (Rotation > RotationGoal)
             {
-                Rotation += RetractSpeed * Time.deltaTime;
-                if (Rotation > 0)
+                Rotation -= Mathf.Abs(RotationGoal - Rotation) * RotateSpeed * Time.deltaTime;
+                if (Rotation <= RotationGoal)
                 {
-                    Rotation = 0;
+                    Rotation = RotationGoal;
                 }
             }
+            else if (Rotation < RotationGoal)
+            {
+                Rotation += Mathf.Abs(RotationGoal - Rotation) * RotateSpeed * Time.deltaTime;
+                if (Rotation >= RotationGoal)
+                {
+                    Rotation = RotationGoal;
+                }
+            }
+
+            transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, Rotation);
         }
+
+
+
         
 
+        /*
         if (Input.GetKeyDown(KeyCode.Q) && !Occupied)
         {
             Occupied = true;
             TiltingL = true;
         }
-        if (Input.GetKey(KeyCode.Q) && Occupied == true && TiltingL == true)
+        else if (Input.GetKey(KeyCode.Q) && Occupied && TiltingL)
         {
             Occupied = true;
             TiltingL = true;
+            Debug.Log(RotateSpeed * Time.deltaTime);
+            Rotation += RotateSpeed * Time.deltaTime;
+            if (Rotation >= 90)
+            {
+                Debug.Log("FuckWorld");
+                Rotation = 90;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.E) && !Occupied)
+        {
+            Occupied = true;
+            TiltingR = true;
+        }
+        else if (Input.GetKey(KeyCode.E) && Occupied && TiltingR)
+        {
+            Occupied = true;
+            TiltingR = true;
+            Debug.Log(RotateSpeed * Time.deltaTime);
+            Rotation -= RotateSpeed * Time.deltaTime;
+            if (Rotation <= -90)
+            {
+                Debug.Log("FuckWorld");
+                Rotation = -90;
+            }
         }
         else
         {
-            Occupied = false;
-            TiltingL = false;
+            FreeToMove();
         }
+        */
 
 
-        transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, Rotation);
     }
 }
