15e5da8df8bfd1591a01b35ff22084493866b888
George Machado <gm2564@nyu.edu>
2019-11-10 17:01:56 -0500

    bomb bool, bool bomb

---
+++ Assets/Scripts/HealthManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthManager : MonoBehaviour
+{
+
+    public Image health;
+    public Image energy;
+
+    public Forward player;
+
+    private float _energy = 50f;
+    private float _health = 100f;
+    private float _maxEnergy = 50f;
+    private float _maxHealth = 100f;
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DrainEnergy()
+    {
+        //if(player.boost==t)
+    }
+    
+    
+    
+}
