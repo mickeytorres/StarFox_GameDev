95b699982428e214b339ffee377b621e65abf18e
Primary <yw3726@nyu.com>
2019-11-11 02:39:21 -0500

    Added Placeholder Enemy, Prefabs and scripts, also, I made NeoPlayer's collider physical instead of trigger for bug fix. DON'T MAKE IT TRIGGER YET.

---
+++ Assets/Scripts/PlaceHolderEnemy/EnemyBulletSpawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBulletSpawner : MonoBehaviour
+{
+    public float timer = 0;
+    public Transform Player;
+    public GameObject Bullet;
+
+    void Update()
+    {
+        transform.LookAt(Player);
+        timer += Time.deltaTime;
+        if (timer > 4)
+        {
+            Instantiate(Bullet,transform.position,transform.rotation);
+            timer = 0;
+        }
+    }
+}
