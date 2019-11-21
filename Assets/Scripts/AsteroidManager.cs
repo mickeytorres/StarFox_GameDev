40fb597345d42086bf989b0f793a50a66a85f83c
George Machado <gm2564@nyu.edu>
2019-11-20 22:03:48 -0500

    added folder for asteroid models

--- Assets/Scripts/AsteroidManager.cs
+++ Assets/Scripts/AsteroidManager.cs
@@ -29,7 +29,7 @@ public class AsteroidManager : MonoBehaviour
     //Asteroid direction
     Vector3 direction = new Vector3(0f,0f,-1f);
 
-    public bool bombRange; 
+    public bool inbombRange; 
         
     
     
@@ -82,31 +82,7 @@ public class AsteroidManager : MonoBehaviour
         }
     }
 
-    public void DamageCalculator()
-    {
-        /*
-        if (_shot.singlePrefab)
-        {
-            _health -= _shot.singleDamage;
-        }
-        else if (_shot.doubleGPrefab)
-        {
-            _health -= _shot.doubleGDamage;
-        }
-        else if (_shot.doubleBPrefab)
-        {
-            _health -= _shot.doubleBDamage;
-        }
-        else if (_shot.chargedPrefab)
-        {
-            _health -= _shot.chargedDamage;
-        }
-        else if (_shot.bombPrefab)
-        {
-            _health -= _shot.bombDamage;
-        }
-        */
-    }
+  
 
     //Destroys object and instantiates 3 smaller objects
     private void Explosion()
@@ -127,8 +103,6 @@ public class AsteroidManager : MonoBehaviour
         }
        
     }
-
-
-
+    
 
 }
