3cdbe4ffa0186508e43e9d0d0c8ca3e367453543
Primary <yw3726@nyu.com>
2019-11-23 23:23:30 -0500

    Made FollowPlayerNormalSpeed Made George's Forward player speed settings public and accessable.

--- Assets/Scripts/Enemies/EnemySpawner.cs
+++ Assets/Scripts/Enemies/EnemySpawner.cs
@@ -7,11 +7,20 @@ public class EnemySpawner : MonoBehaviour
     public GameObject Player;
 
     public GameObject FirstWaveButterfly;
+    public float FirstWaveButterfly_SpawnZpos;
     public bool Has_FirstWaveButterfly_Spawned = false;
 
     public GameObject Mech;
+    public float Mech_SpawnZpos;
+    public bool Has_Mech_Spawned = false;
+
     public GameObject FirstWaveWorm;
+    public float FirstWaveWorm_SpawnZpos;
+    public bool Has_FirstWaveWorm_Spawned = false;
+
     public GameObject ConeHeads;
+    public float ConeHeads_SpawnZpos;
+    public bool Has_ConeHeads_Spawned = false;
 
     void Start()
     {
@@ -21,7 +30,7 @@ public class EnemySpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Player.transform.position.z >= 20 && !Has_FirstWaveButterfly_Spawned)
+        if (Player.transform.position.z >= FirstWaveButterfly_SpawnZpos && !Has_FirstWaveButterfly_Spawned)
         {
             Instantiate(FirstWaveButterfly);
             Has_FirstWaveButterfly_Spawned = true;
