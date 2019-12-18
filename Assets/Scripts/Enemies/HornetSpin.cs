087a936d3db814aacf3c446d2917384d79c47599
Primary <yw3726@nyu.com>
2019-12-18 18:34:41 -0500

    Hornet and afterburner

---
+++ Assets/Scripts/Enemies/HornetSpin.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HornetSpin : MonoBehaviour
+{
+
+    void Update()
+    {
+        transform.Rotate(0,720*Time.deltaTime,0);
+    }
+}
